26d95d170257c26fb4f829d5c9aae0c3d570f942
danilodurante <60218775+danilodurante@users.noreply.github.com>
2025-12-08 15:52:13 +0100

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // frontend dev
+    credentials: true,               // permette invio cookie
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3001);
 }
